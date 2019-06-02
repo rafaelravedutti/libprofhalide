d651d515bf00429bdf30efb7400bc55e2f40537a
Rafael Ravedutti <rafael@laura-br.com>
2019-06-02 03:36:47 -0300

    Adjust indent
    Signed-off-by: Rafael Ravedutti <rafael@laura-br.com>

--- tests/blur/plot_charts.py
+++ tests/blur/plot_charts.py
@@ -14,46 +14,46 @@ def get_file_results(filename, profile_file, image_size, schedule):
     profile_iter = None
 
   with open(filename) as csvfile:
-      csv_data = csv.reader(csvfile, delimiter=',')
-      time_results = True
+    csv_data = csv.reader(csvfile, delimiter=',')
+    time_results = True
 
-      for row in csv_data:
-        columns_number = len(row)
+    for row in csv_data:
+      columns_number = len(row)
 
-        if columns_number > 1:
-          stage = row[0]
-          column = 0
+      if columns_number > 1:
+        stage = row[0]
+        column = 0
 
-          if time_results and stage not in time_data:
-            time_data[stage] = {}
-            time_iter[stage] = {}
+        if time_results and stage not in time_data:
+          time_data[stage] = {}
+          time_iter[stage] = {}
 
-          if not time_results and stage not in profile_data:
-            profile_data[stage] = {}
-            profile_iter[stage] = {}
+        if not time_results and stage not in profile_data:
+          profile_data[stage] = {}
+          profile_iter[stage] = {}
 
-          for data in row[1:columns_number]:
-            data = float(data)
+        for data in row[1:columns_number]:
+          data = float(data)
 
-            if time_results:
-              if column not in time_data[stage]:
-                time_data[stage][column] = data
-                time_iter[stage][column] = 1
-              else:
-                time_data[stage][column] += data
-                time_iter[stage][column] += 1
+          if time_results:
+            if column not in time_data[stage]:
+              time_data[stage][column] = data
+              time_iter[stage][column] = 1
             else:
-              if column not in profile_data[stage]:
-                profile_data[stage][column] = data
-                profile_iter[stage][column] = 1
-              else:
-                profile_data[stage][column] += data
-                profile_iter[stage][column] += 1
+              time_data[stage][column] += data
+              time_iter[stage][column] += 1
+          else:
+            if column not in profile_data[stage]:
+              profile_data[stage][column] = data
+              profile_iter[stage][column] = 1
+            else:
+              profile_data[stage][column] += data
+              profile_iter[stage][column] += 1
 
-            column += 1
+          column += 1
 
-        elif profile_file:
-          time_results = not time_results
+      elif profile_file:
+        time_results = not time_results
 
   return {
     'image_size': image_size,
