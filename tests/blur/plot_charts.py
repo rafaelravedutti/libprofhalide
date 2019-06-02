f797759abe3161420b10ea0dce4210ee83cb83cf
Rafael Ravedutti <rafael@laura-br.com>
2019-06-02 03:59:05 -0300

    Put total result in top of bars
    Signed-off-by: Rafael Ravedutti <rafael@laura-br.com>

--- tests/blur/plot_charts.py
+++ tests/blur/plot_charts.py
@@ -136,6 +136,11 @@ for i in range(0, counter, len(schedules)):
   blur_y_flop_array = []
   blur_y_data_volume_array = []
 
+  total_time_array = []
+  total_cache_miss_array = []
+  total_flop_array = []
+  total_data_volume_array = []
+
   for j in range(len(schedules)):
     tdata = time_results[i + j]['time_data']
     titer = time_results[i + j]['time_iter']
@@ -153,6 +158,12 @@ for i in range(0, counter, len(schedules)):
     blur_y_flop_avg = 0
     blur_y_lines_out_avg = 0
     blur_y_rqsts_miss_avg = 0
+
+    total_time_avg = tdata['all'][0] / titer['all'][0]
+    total_cache_miss_avg = pdata['total'][0] / piter['total'][0]
+    total_flop_avg = pdata['total'][1] / piter['total'][1]
+    total_lines_out_avg = pdata['total'][2] / piter['total'][2]
+    total_rqsts_miss_avg = pdata['total'][3] / piter['total'][3]
 
     for stage in tdata:
       if stage == 'blur_x':
@@ -184,6 +195,11 @@ for i in range(0, counter, len(schedules)):
     blur_y_flop_array.append(blur_y_flop_avg)
     blur_y_data_volume_array.append(1.E-6 * (blur_y_lines_out_avg + blur_y_rqsts_miss_avg) * 64)
 
+    total_time_array.append(total_time_avg)
+    total_cache_miss_array.append(total_cache_miss_avg)
+    total_flop_array.append(total_flop_avg)
+    total_data_volume_array.append(1.E-6 * (total_lines_out_avg + total_rqsts_miss_avg) * 64)
+
   p1 = plt.bar(y_pos, blur_x_time_array, align='center', alpha=0.5)
   p2 = plt.bar(y_pos, blur_y_time_array, align='center', alpha=0.5, bottom=blur_x_time_array)
   plt.xticks(y_pos, schedules)
@@ -191,6 +207,13 @@ for i in range(0, counter, len(schedules)):
   plt.ylabel("Time (ms)")
   plt.title("Execution time per schedule for {} image".format(time_results[i]['image_size']))
   plt.legend((p1[0], p2[0]), ('blur_x', 'blur_y'))
+
+  bar_counter = 0
+  for bar in p2:
+    tot = total_time_array[bar_counter]
+    plt.text(bar.get_x() + 0.1, tot + 0.5, int(tot))
+    bar_counter += 1
+
   plt.show()
 
   p1 = plt.bar(y_pos, blur_x_cache_miss_array, align='center', alpha=0.5)
@@ -200,6 +223,13 @@ for i in range(0, counter, len(schedules)):
   plt.ylabel("Cache misses")
   plt.title("L1 Cache Misses per schedule for {} image".format(time_results[i]['image_size']))
   plt.legend((p1[0], p2[0]), ('blur_x', 'blur_y'))
+
+  bar_counter = 0
+  for bar in p2:
+    tot = total_cache_miss_array[bar_counter]
+    plt.text(bar.get_x() + 0.1, tot + 0.5, int(tot))
+    bar_counter += 1
+
   plt.show()
 
   p1 = plt.bar(y_pos, blur_x_flop_array, align='center', alpha=0.5)
@@ -209,6 +239,13 @@ for i in range(0, counter, len(schedules)):
   plt.ylabel("FLOP")
   plt.title("FLOP per schedule for {} image".format(time_results[i]['image_size']))
   plt.legend((p1[0], p2[0]), ('blur_x', 'blur_y'))
+
+  bar_counter = 0
+  for bar in p2:
+    tot = total_flop_array[bar_counter]
+    plt.text(bar.get_x() + 0.1, tot + 0.5, int(tot))
+    bar_counter += 1
+
   plt.show()
 
   p1 = plt.bar(y_pos, blur_x_data_volume_array, align='center', alpha=0.5)
@@ -218,4 +255,11 @@ for i in range(0, counter, len(schedules)):
   plt.ylabel("Data volume (Mb)")
   plt.title("L3 Data Volume per schedule for {} image".format(time_results[i]['image_size']))
   plt.legend((p1[0], p2[0]), ('blur_x', 'blur_y'))
+
+  bar_counter = 0
+  for bar in p2:
+    tot = total_data_volume_array[bar_counter]
+    plt.text(bar.get_x() + 0.1, tot + 0.5, int(tot))
+    bar_counter += 1
+
   plt.show()
