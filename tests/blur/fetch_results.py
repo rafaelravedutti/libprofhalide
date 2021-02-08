889ced9f6a369519219cab1db51591f2e4a5c087
Rafael Ravedutti <rafaelravedutti@gmail.com>
2021-02-08 18:47:01 +0100

    Add python script to fetch results
    Signed-off-by: Rafael Ravedutti <rafaelravedutti@gmail.com>

---
+++ tests/blur/fetch_results.py
@@ -0,0 +1,86 @@
+from os import listdir
+from os.path import isfile, join
+import re
+
+path_prefix="results/cascadelake/blur/10240x4320x3"
+path_suffix="_serial.txt"
+
+def avg(lst):
+    return sum(lst) / len(lst)
+
+def first(lst):
+    return lst[0]
+
+def num(s):
+    try:
+        return int(s)
+    except ValueError:
+        return float(s)
+
+# Group, Regex, Column, Reduce and Policy functions
+events=[
+    ("MEM",      "Memory bandwidth",                         2, avg, first),
+    ("CACHES",   "L1 to L2 evict data volume",               2, sum, first),
+    ("CACHES",   "L2 to L1 load data volume",                2, sum, first),
+    ("CACHES",   "L1D_REPLACEMENT",                          3, sum, min),
+    ("FLOPS_SP", "AVX SP",                                   2, sum, first),
+    ("FLOPS_SP", "FP_ARITH_INST_RETIRED_256B_PACKED_SINGLE", 3, sum, min)
+]
+
+region_pattern = re.compile("^Region")
+results = {}
+event_id = 0
+
+for event in events:
+    group = event[0]
+    regex = event[1]
+    column = event[2]
+    reduce_fn = event[3]
+    policy_fn = event[4]
+
+    pattern = re.compile(regex)
+    path = path_prefix + "/" + group
+    files = [f for f in listdir(path) if isfile(join(path, f)) and f.endswith(path_suffix)]
+
+    for f in files:
+        schedule, _ = f.split('.')
+        filename = path + "/" + f
+
+        if schedule not in results:
+            results[schedule] = {}
+
+        schedule_results = results[schedule]
+
+        with open(filename, 'r') as fp:
+            region = None
+            for line in fp.readlines():
+                if region_pattern.search(line):
+                    region, _ = line[7:].split(',')
+
+                if pattern.search(line):
+                    value = num(line.split('|')[column].strip())
+
+                    if region not in schedule_results:
+                        schedule_results[region] = {}
+
+                    region_results = schedule_results[region]
+                    if event_id not in region_results:
+                        region_results[event_id] = value
+                    else:
+                        region_results[event_id] = policy_fn([region_results[event_id], value])
+
+    event_id += 1
+
+
+for schedule in results:
+    schedule_results = results[schedule]
+    for region in schedule_results:
+        region_results = schedule_results[region]
+        output = schedule + "," + region
+
+        for event_id in region_results:
+            value = region_results[event_id]
+            output += "," + str(value)
+
+        print(output)
+
